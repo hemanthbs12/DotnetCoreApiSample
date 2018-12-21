eb6be8636aba44db450b4838a0795fc8378d4838
Hemanth BS <hemanth.bs@euromonitor.com>
2018-12-21 18:59:14 +0530

    Add project files.

---
+++ DotnetCoreApiSample/Controllers/TodoController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DotnetCoreApiSample.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotnetCoreApiSample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        public TodoController(TodoContext context)
+        {
+            TodoContext context1 = context;
+
+            if (context1.TodoItems.Count() == 0)
+            {
+                // Create a new TodoItem if collection is empty,
+                // which means you can't delete all TodoItems.
+                context1.TodoItems.Add(new TodoItem { Name = "Item1" });
+                context1.SaveChanges();
+            }
+        }
+    }
+}
